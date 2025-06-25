8eb1e69de3743aa7cb329873c6a3e1e39bd16030
George Soteriou <george.soteriou.1997@gmail.com>
2025-06-25 11:17:35 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ set -o nounset  # fail on undef var
 set -o pipefail # fail on first error in pipe
 
 curl --silent --fail --show-error --location --remote-name --remote-header-name\
-  https://github.com/PiotrMachowski/Xiaomi-cloud-tokens-extractor/releases/latest/download/token_extractor.zip
+  https://github.com/georgesoteriou/Xiaomi-cloud-tokens-extractor/releases/latest/download/token_extractor.zip
 unzip token_extractor.zip
 cd token_extractor
 pip3 install -r requirements.txt
